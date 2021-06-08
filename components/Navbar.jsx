2a8d9d52c9ebff76995abc2bdd19e5f683edeb50
wonkatin <j.drawers@gmail.com>
2021-06-08 17:51:26 -0500

    nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,13 +2,24 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link href='/'>
+        <nav className="stayput">
+            <Link href='#home'>
                 <a>Home</a>
             </Link>
-            <Link href='/about'>
-                <a>About</a>
+            <Link href='#about'>
+                <a>About me</a>
             </Link>
+            <Link href='#skillz'>
+                <a>Skills</a>
+            </Link>
+            <Link href='#projects'>
+                <a>Projects</a>
+            </Link>
+            <Link href='#contact'>
+                <a>Contact</a>
+            </Link>
+
+
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,32 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>My CAT portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+      <div className={styles.container}>
+            <Head>
+              <title>Sarah Marie Coogan</title>
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <h1> Welcome to home page</h1>
-      
-    </div>
-  )
+            <div className={styles.container} id="home">
+              <h1>Sarah Marie Coogan</h1>
+              <p></p>
+            </div>
+
+            <div className={styles.container} id="about">
+                <h2>About Me</h2>
+            </div>
+            <div className={styles.container} id="skillz">
+                <h2>Skills</h2>
+            </div>
+            <div className={styles.container} id="projects">
+                <h2>Projects</h2>
+            </div>
+            <div className={styles.container} id="contact">
+                <h2>Contact Info</h2>
+            </div>
+
+
+      </div>
+    )
 }
