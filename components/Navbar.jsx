08db7c1b9f0a39726e6a2385c3e5b4fcd3fce024
wonkatin <j.drawers@gmail.com>
2021-06-09 11:03:58 -0500

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        <nav className="stayput">
-            <Link href='#home'>
+        <nav className="stay-put">
+            <Link href='#home' >
                 <a>Home</a>
             </Link>
-            <Link href='#about'>
-                <a>About me</a>
+            <Link href='#tech'>
+                <a>Tech</a>
             </Link>
-            <Link href='#skillz'>
+            {/* <Link href='#skillz'>
                 <a>Skills</a>
-            </Link>
+            </Link> */}
             <Link href='#projects'>
                 <a>Projects</a>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,74 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
-      <div className={styles.container}>
+      <div className="container">
             <Head>
               <title>Sarah Marie Coogan</title>
               <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div className={styles.container} id="home">
-              <h1>Sarah Marie Coogan</h1>
-              <p></p>
+            <div className="container" id="home">
+              <div className="name-blast">Sarah Marie Coogan</div>
+              <p className="job-title">Data Visualization Engineer</p>
             </div>
 
-            <div className={styles.container} id="about">
-                <h2>About Me</h2>
+            <div className="container secondary" id="tech">
+                <h1>Tech Skills</h1>
             </div>
-            <div className={styles.container} id="skillz">
-                <h2>Skills</h2>
+
+            {/* <div className="container secondary" id="skillz">
+                <h1>Skills</h1>
+            </div> */}
+
+            <div className="container secondary" id="projects">
+                <h1>Projects</h1>
             </div>
-            <div className={styles.container} id="projects">
-                <h2>Projects</h2>
-            </div>
-            <div className={styles.container} id="contact">
-                <h2>Contact Info</h2>
+
+            <div className="container secondary" id="contact">
+                <h1>Contact Me!</h1>
+                <div className="content">
+                    <div className="img">
+
+                        <Image
+                            src='/sarah.png'
+                            alt='a picture of Sarah Marie Coogan'
+                            height={300}
+                            width={300}
+                        />
+                    </div>
+                    <div className="deets">
+                        <ul>
+                            <li>
+                                E-mail: SarahMarieCoogan@gmail.com
+                            </li>
+                            <li>
+                                <Link href='https://github.com/wonkatin'>
+                                    <a>GitHub</a>
+                                </Link> 
+                            </li>
+                            <li>
+                                <Link href='https://www.linkedin.com/in/sarahmariecoogan/'>
+                                    <a>LinkedIn</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='https://docs.google.com/document/d/1ONbbax_CHdMejgWQ9P9cy8yW5rmX68lCNmk09k7JOa0/edit?usp=sharing'>
+                                    <a>Resume</a>
+                                </Link>
+                            </li>
+                            {/* <li>
+                                <Link href='https://codepen.io/wonkatin'>
+                                    <a>Code Pen</a>
+                                </Link>
+                            </li> */}
+                        </ul>
+                    </div>
+                </div>
+
             </div>
 
 
