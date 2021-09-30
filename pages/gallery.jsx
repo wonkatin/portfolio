31a424cc928cd7f6acbf7002e66ac302efddfe6d
wonkatin <j.drawers@gmail.com>
2021-09-29 23:15:26 -0500

    disable gallery temporarily

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -32,68 +32,6 @@ function ArrowRight(props) {
     )
 }
 
-const designData = [
-    {
-        image: '../public/artwork/design/0.jpg'
-    },
-    {
-        image: '../public/artwork/design/1.jpg'
-    },
-    {
-        image: '../public/artwork/design/2.jpg'
-    },
-    {
-        image: '../public/artwork/design/3.jpg'
-    },
-    {
-        image: '../public/artwork/design/4.jpg'
-    },
-    {
-        image: '../public/artwork/design/5.jpg'
-    },
-    {
-        image: '../public/artwork/design/6.jpg'
-    },
-    {
-        image: '../public/artwork/design/7.jpg'
-    },
-    {
-        image: '../public/artwork/design/8.jpg'
-    },
-    {
-        image: '../public/artwork/design/9.jpg'
-    },
-    {
-        image: '../public/artwork/design/10.jpg'
-    },
-    {
-        image: '../public/artwork/design/11.jpg'
-    },
-    {
-        image: '../public/artwork/design/12.jpg'
-    },
-    {
-        image: '../public/artwork/design/13.jpg'
-    },
-    {
-        image: '../public/artwork/design/14.jpg'
-    },
-    {
-        image: '../public/artwork/design/15.jpg'
-    },
-    {
-        image: '../public/artwork/design/16.jpg'
-    },
-    {
-        image: '../public/artwork/design/17.jpg'
-    },
-    {
-        image: '../public/artwork/design/18.jpg'
-    },
-    {
-        image: '../public/artwork/design/19.jpg'
-    },
-]
 export default function Gallery() {
     const [currentSlide, setCurrentSlide] = React.useState(0)
     const [sliderRef, slider] = useKeenSlider({
@@ -109,42 +47,47 @@ export default function Gallery() {
             <div className="gallery">
                 <div className="navigation-wrapper">
                     <div ref={sliderRef} className="keen-slider">
-                        <div className="keen-slider__slide number-slide1">1</div>
-                        <div className="keen-slider__slide number-slide2">2</div>
-                        <div className="keen-slider__slide number-slide3">3</div>
-                        <div className="keen-slider__slide number-slide4">4</div>
-                        <div className="keen-slider__slide number-slide5">5</div>
-                        <div className="keen-slider__slide number-slide6">6</div>
+                        <div className="keen-slider__slide">
+                            <Image
+                                loading="eager"
+                                // className="pic"
+                                src='/../public/artwork/design/0.jpg'
+                                // alt='a picture of Kitchen Sink app, an app that searches for recipes'
+                                height="788"
+                                width="600"
+                            />
+                        </div>
+                        <div className="keen-slider__slide">2</div>
+                        <div className="keen-slider__slide">3</div>
+                        <div className="keen-slider__slide">4</div>
+                        <div className="keen-slider__slide">5</div>
+                        <div className="keen-slider__slide">6</div>
+                        <div className="keen-slider__slide">7</div>
+                        <div className="keen-slider__slide">8</div>
+                        <div className="keen-slider__slide">9</div>
+                        <div className="keen-slider__slide">10</div>
+                        <div className="keen-slider__slide">11</div>
+                        <div className="keen-slider__slide">12</div>
+                        <div className="keen-slider__slide">13</div>
+                        <div className="keen-slider__slide">14</div>
+                        <div className="keen-slider__slide">15</div>
+                        <div className="keen-slider__slide">16</div>
+                        <div className="keen-slider__slide">6</div>
+                        <div className="keen-slider__slide">6</div>
+                        <div className="keen-slider__slide">6</div>
+                        <div className="keen-slider__slide">6</div>
                     </div>
                     {slider && (
                       <>
                         <ArrowLeft
                           onClick={(e) => e.stopPropagation() || slider.prev()}
-                          disabled={currentSlide === 0}
                         />
                         <ArrowRight
                           onClick={(e) => e.stopPropagation() || slider.next()}
-                          disabled={currentSlide === slider.details().size - 1}
                         />
                       </>
                     )}
                 </div>
-                {slider && (
-                  <div className="dots">
-                    {[...Array(slider.details().size).keys()].map((idx) => {
-                      return (
-                        <button
-                          key={idx}
-                          onClick={() => {
-                            slider.moveToSlideRelative(idx)
-                          }}
-                          className={"dot" + (currentSlide === idx ? " active" : "")}
-                        />
-                      )
-                    })}
-                  </div>
-                )}
-    
             </div>
         </div>
     )
